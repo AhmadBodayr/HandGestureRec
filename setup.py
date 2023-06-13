d08ff49e8542890047987665b2a1ca784a947bd1
Ahmad Bodayr <ahmadbodayr@Ahmads-MacBook-Pro.local>
2023-06-14 01:23:27 +0300

    edited the setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
 from setuptools import setup, find_packages
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 setup(
+    long_description=long_description, 
+    long_description_content_type="text/,markdown",
     name='HandGestureRecSiliconM1',
     version='0.1',
     description='m1 macs version',
